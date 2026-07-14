5a725b30bad721e7a955f78c5ed37ec608b74574
jchaeyun <jocheyoon@gmail.com>
2026-07-14 20:45:08 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

---
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+
+
+using namespace std;
+
+int solution(string s) {
+    
+    vector<string> words={
+        "zero","one","two","three","four","five","six","seven","eight","nine"
+    };
+    
+    for(int i=0;i<10;i++){
+        size_t pos=0;
+        while((pos=s.find(words[i],pos))!=string::npos){
+            s.replace(pos,words[i].length(),to_string(i));
+            pos=+1;
+        }
+    }
+    
+    return stoi(s);
+}
