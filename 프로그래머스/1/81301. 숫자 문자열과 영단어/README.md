590f01c95ee675797e6af5cb68b90d6ad4d218d7
jchaeyun <jocheyoon@gmail.com>
2026-07-23 21:00:36 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/README.md
@@ -1,6 +1,6 @@
 # [level 1] 숫자 문자열과 영단어 - 81301 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301?language=cpp) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/81301) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2026년 07월 14일 20:45:05
+2026년 07월 23일 21:00:34
 
 ### 문제 설명
 
--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.cpp
@@ -6,17 +6,18 @@ using namespace std;
 
 int solution(string s) {
     
-    vector<string> words={
-        "zero","one","two","three","four","five","six","seven","eight","nine"
-    };
+  vector<string> words={"zero","one","two","three","four","five","six","seven","eight","nine"};
     
-    for(int i=0;i<10;i++){
+   
+    for(int i=0;i<words.size();i++){ //벡터 배열은 length()가 아니라 size()
         size_t pos=0;
         while((pos=s.find(words[i],pos))!=string::npos){
             s.replace(pos,words[i].length(),to_string(i));
-            pos=+1;
         }
     }
     
+        
+    
     return stoi(s);
-}
+}   
+    
