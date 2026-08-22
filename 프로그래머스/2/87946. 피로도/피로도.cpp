28b41a79d0eb0c89704c976ce10ab85e8183249b
jchaeyun <jocheyoon@gmail.com>
2026-08-22 22:45:49 +0900

    [level 2] Title: 피로도, Time: 0.57 ms, Memory: 4.82 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.cpp
+++ 프로그래머스/2/87946. 피로도/피로도.cpp
@@ -1,37 +1,37 @@
 #include <string>
 #include <vector>
-#include <algorithm>
 
 using namespace std;
-int answer=0; 
-void dfs(int depth,int k, vector<bool>& visited,const vector<vector<int>>& dungeons){
-    answer=max(answer,depth);//던전 수
+
+int answer=0;
+
+void dfs(int k, const vector<vector<int>>& dungeons,vector<bool>&visited,int cnt){
     
+    answer=max(cnt,answer); //최대 던전 수 기록. cnt는 지난 경로의 던전 수, answer는 지금까지 최대 던전 수
+    
+    if(k<=0) return; //종료 조건
+    
+    //다음에 방문할 던전 고르기
     for(int i=0;i<dungeons.size();i++){
+        
         if(visited[i]) continue;
-        if(k<dungeons[i][0]) continue;
         
-        
-                visited[i]=true; 
-                dfs(depth+1,k-dungeons[i][1],visited,dungeons);
-                visited[i]=false;
-        
+        if(k>=dungeons[i][0]){
+            visited[i]=true;
+            dfs(k-dungeons[i][1],dungeons,visited,cnt+1);
+            visited[i]=false;
+        }
         
     }
-    
-    
 }
 
 int solution(int k, vector<vector<int>> dungeons) {
-    //그리디로 풀리는 문제면 8개라는 제한을 안둠->완전탐색 하라는 얘기
-    //최소 피로도<=k인 경우 그 던전 돌고 count++ 아니면 건너뜀
-    //visited 배열로 체크하기
+    //완전탐색 순열
+   
+    int cnt=0;
     vector<bool> visited(dungeons.size(),false);
-    dfs(0,k,visited,dungeons);
+    
+    dfs(k,dungeons,visited,cnt);
     
     return answer;
-    
-    
-    
-    
 }
