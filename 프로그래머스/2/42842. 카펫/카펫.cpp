6c02cd61d537abb30cbd49ac5c11bf0153af19ad
jchaeyun <jocheyoon@gmail.com>
2026-08-24 01:09:09 +0900

    [level 2] Title: 카펫, Time: 0.01 ms, Memory: 4.95 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -2,38 +2,32 @@
 #include <vector>
 
 using namespace std;
-//약수 쌍
+
 vector<int> solution(int brown, int yellow) {
     int total=brown+yellow;
-    int row=3;
+    int row=3; //세로
+    int col=3; //가로
     
-    int col=total/row;
-    
-    int b=2*(col+row-2);
-    //48 3 16 34->
-    
-    while(col>=row){
-        
-        if(total%row!=0){
-            row++;
-                
-        }else{
-            col=total/row;
-            b=2*(col+row-2);
-
-            if(b==brown){
-                return {col,row};
-            }else{
-                row++;
-            }
-
-            
+    while(true){
+        if(total%col!=0){
+            col++;
+            continue;
         }
+        
+        row=total/col;
+        
+        if(col<row){
+            col++;
+           continue;
+        }
+        
+        if(brown-2*(col+row-2)){
+            col++;
+            continue;
+        }
+           return {col,row};  
         
         
     }
     
-    return {};
-        
-}   
-    
+}
