235d2fc3c35d2784a5eb8070b8f42d2e47603fec
jchaeyun <jocheyoon@gmail.com>
2026-08-26 00:56:35 +0900

    [level 2] Title: 땅따먹기, Time: 3.95 ms, Memory: 26.3 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.cpp
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.cpp
@@ -1,27 +1,22 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-
 using namespace std;
-
+//완전탐색,dp->여기까지의 최적값을 저장
 int solution(vector<vector<int> > land)
 {
+    int r=land.size(); //행 개수
     
-   
-        for (int i = 1; i < land.size(); i++) {
-        land[i][0] += max({land[i-1][1], land[i-1][2], land[i-1][3]});
-        land[i][1] += max({land[i-1][0], land[i-1][2], land[i-1][3]});
-        land[i][2] += max({land[i-1][0], land[i-1][1], land[i-1][3]});
-        land[i][3] += max({land[i-1][0], land[i-1][1], land[i-1][2]});
-        }
-            
-          
     
-    int r=land.size()-1;
-   
-    return max({land[r][1],land[r][2],land[r][3],land[r][0]});
-
-    // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
-   
-
+    for(int i=1;i<r;i++){
+        land[i][0]+=max({land[i-1][1],land[i-1][2],land[i-1][3]});
+        land[i][1]+=max({land[i-1][0],land[i-1][2],land[i-1][3]});
+        land[i][2]+=max({land[i-1][1],land[i-1][0],land[i-1][3]});
+        land[i][3]+=max({land[i-1][1],land[i-1][2],land[i-1][0]});
+        
+    }
+    
+    int ans=max({land[r-1][0],land[r-1][1],land[r-1][2]});
+    return max(ans,land[r-1][3]);
+    
 }
