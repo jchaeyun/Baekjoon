5d30b0d6a3ad07227f7acd9fb88546229443435a
jchaeyun <jocheyoon@gmail.com>
2026-09-03 18:25:52 +0900

    [level 2] Title: 프로세스, Time: 0.03 ms, Memory: 4.83 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.cpp
+++ 프로그래머스/2/42587. 프로세스/프로세스.cpp
@@ -1,42 +1,35 @@
 #include <string>
 #include <vector>
-#include <queue>
-#include <utility>
-
+#include <queue> //priority_queue
+#include <algorithm> //sort
 
 using namespace std;
 
 int solution(vector<int> priorities, int location) {
-    
+    int ans=0;
+    vector<int> v;
     queue<pair<int,int>> q;
-   
-    priority_queue<int> pq; //자동 내림차순 정렬
-  
-  
-
-    int count=0;
-    //초기화
     for(int i=0;i<priorities.size();i++){
-        q.push({priorities[i],i}); //{우선순위,원래위치}
-        pq.push(priorities[i]);
+        v.push_back(priorities[i]);
+        q.push({priorities[i],i});//큐 q는 {priorities,idx}
     }
+    //priorities를 내림차순으로 정렬한 v
+    sort(v.begin(),v.end(),greater<int>()); 
     
-    while(!q.empty()){ 
-        pair<int,int> cur=q.front(); //auto cur = q.front();
-        q.pop(); //큐에서 꺼냄
-        
-        if(cur.first<pq.top()){
-        q.push(cur); //뒤로 보냄
-      
+    //v랑 일치하지 않으면 뒤로 보냄 
+    int i=0;
+    while(!q.empty()){
+        auto [p,idx]=q.front(); q.pop();
+        if(v[i]!=p){
+           q.push({p,idx});
         }else{
-        pq.pop(); //현재 프로세스 실행
-        
-        count++; //실행 순서
-        if(cur.second==location){
-            return count;
-        }
+            ans++;
+            i++;
+            if(idx==location) return ans;
+            
         }
     }
+    //v랑 일치하면 pop,ans++ 그리고 이게 idx도 location이랑 일치하면 return ans;
     
     
 }
