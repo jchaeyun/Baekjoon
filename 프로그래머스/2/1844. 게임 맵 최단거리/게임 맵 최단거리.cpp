0c1dff57939d65ff8b1f2780cccc9647937f978c
jchaeyun <jocheyoon@gmail.com>
2026-09-05 22:57:41 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 0.23 ms, Memory: 4.44 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.cpp
@@ -2,38 +2,43 @@
 #include <queue>
 using namespace std;
 
-int solution(vector<vector<int> > maps)
+int solution(vector<vector<int>> maps)
 {
-    //maps가 1이면 가능, 0이면 못감
-    int dy[4]={-1,1,0,0};
-    int dx[4]={0,0,-1,1};
+    //n행 m열
+    //0은 못지나가고 1이면 갈수있음
+    //시작위치:1,1 도착지:n,m. 못다다르면 
+    //bfs
+    int dx[4]={-1,1,0,0};
+    int dy[4]={0,0,-1,1};
     
-    int n=maps.size(); //행 개수
-    int m=maps[0].size(); //열 개수
+    int n=maps.size(); //y,행
+    int m=maps[0].size(); //x, 열
+    
     queue<pair<int,int>> q;
-    vector<vector<bool>> visited(n,vector<bool>(m,false));
-    vector<vector<int>> dist(n,vector<int>(m,-1));
+    vector<vector<int>> visited(n,vector<int>(m,1));
+    
     q.push({0,0});
-    dist[0][0]=1;
-    
-    
+  
     while(!q.empty()){
-        
         auto [y,x]=q.front(); q.pop();
+        if(y==n-1&&x==m-1) break;
+        
         for(int i=0;i<4;i++){
-            int ny=y+dy[i];
-            int nx=x+dx[i];
-            
-            if(ny>=n||ny<0||nx>=m||nx<0||maps[ny][nx]==0) continue;
-            if(visited[ny][nx]) continue;
-            visited[ny][nx]=true;
-            dist[ny][nx]=dist[y][x]+1;
-            if(ny==n-1&&nx==m-1) return dist[n-1][m-1];
-            q.push({ny,nx});
-            
-            
+            int r=y+dy[i];
+            int c=x+dx[i];
+            if(r<0||r>=n||c<0||c>=m) continue;
+            if(visited[r][c]==1&&maps[r][c]==1){
+                visited[r][c]+=visited[y][x];
+                q.push({r,c});
+            }
         }
     }
-    return -1;
+    if(visited[n-1][m-1]==1){
+        return -1;
+    }else{
+      return visited[n-1][m-1];
+    }
     
+    
+   
 }
