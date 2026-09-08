f0baae1c95d8a5125178d8b331bcb4f71f416d1a
jchaeyun <jocheyoon@gmail.com>
2026-09-08 18:10:06 +0900

    [level 2] Title: 땅따먹기, Time: 3.64 ms, Memory: 26.3 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.cpp
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.cpp
@@ -2,21 +2,17 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-//완전탐색,dp->여기까지의 최적값을 저장
+
 int solution(vector<vector<int> > land)
 {
-    int r=land.size(); //행 개수
-    
-    
-    for(int i=1;i<r;i++){
+    int n=land.size();
+    for(int i=1;i<n;i++){
         land[i][0]+=max({land[i-1][1],land[i-1][2],land[i-1][3]});
         land[i][1]+=max({land[i-1][0],land[i-1][2],land[i-1][3]});
         land[i][2]+=max({land[i-1][1],land[i-1][0],land[i-1][3]});
-        land[i][3]+=max({land[i-1][1],land[i-1][2],land[i-1][0]});
-        
+        land[i][3]+=max({land[i-1][1],land[i-1][0],land[i-1][2]});
     }
-    
-    int ans=max({land[r-1][0],land[r-1][1],land[r-1][2]});
-    return max(ans,land[r-1][3]);
-    
+    int maxNum=max({land[n-1][1],land[n-1][0],land[n-1][2],land[n-1][3]});
+   
+    return maxNum;
 }
